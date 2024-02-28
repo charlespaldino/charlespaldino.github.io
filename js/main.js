9e457f4ac6621f83748e77334226ec8dc51189ff
Charles Paldino <charlespaldino@protonmail.com>
2024-02-28 15:40:30 -0500

    refactor for main js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 
+var debugmode = true;
+
+$(document).ready(function () {
+    $('.carousel').carousel('pause');
+
+    $(".nav-link").on("click", function () {
+        $(this).addClass("active")
+    })
+
+    var summary = new Summary();
+    $("#view_summary").html(summary.getHTML());
+    $("#view_summary_mobile").html($("#view_summary").html());
+
+    var workhistory = new WorkHistory();
+    workhistory.getHTML(debugmode, "#view_resume");
+
+    var trainingcerts = new TrainingCert();
+    trainingcerts.getHTML(debugmode, "#view_training");
 
 
-//Set default manager settings.
-SinglePageViewManager.setDefaultView("view_resume");
+    //Nav Links
+    $("#navlink_home").on("click", function () {
+        switchView("section_summary");
+    })
 
+    $("#navlink_resume").on("click", function () {
+        switchView("section_resume");
+    })
+
+    $("#navlink_training").on("click", function () {
+        switchView("section_training");
+    })
+
+    //$("#navlink_live").on("click", function ()
+    //{
+    //    switchView("section_porfolio_live");
+    //})
+
+});
